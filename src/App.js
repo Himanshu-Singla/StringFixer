0fbb643f18ccef5da3c3b3c481a4f426448a8273
Himanshu-Singla <himanshu1572.be20@chitkara.edu.in>
2023-03-01 17:16:44 +0530

    dark mode and new features

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-
 import './App.css';
 import Navbar from './Components/Navbar';
 import Textform from './Components/Textform';
+import React,{useState} from 'react';
+import PropTypes from 'prop-types';
 function App() {
+  const [mode,setMode]=useState('light');
+  const toggleMode = () =>{
+    if(mode==='light')
+    {setMode ('dark') ;
+     document.body.style.backgroundColor = '#061c33';
+  }
+    else{
+      setMode  ('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
         
-    <Navbar title="StringFixer" About="About StringFixer"/>
+    <Navbar title="StringFixer" About="About StringFixer" mode={mode} toggleMode={toggleMode}/>
     <div className="container my-8">
-    <Textform heading = "Enter the text to analyze"/>
+    <Textform heading = "Enter the text to analyze" mode={mode}/>
     </div>
     
 </>
--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -8,20 +8,57 @@ export default function Textform(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const EventDown = () =>{
+      console.log("Lower case Event Achive"+text);
+      let newText1=text.toLowerCase();
+      setText(newText1);
+    }
+    const clear = () =>{
+      console.log("Clear event achive"+text);
+      let newText2 = " ";
+      setText(newText2);
+    }
+    const speak = () => {
+      let msg = new SpeechSynthesisUtterance();
+      msg.text = text;
+      window.speechSynthesis.speak(msg);
+    }
+    const copy = () =>{
+      let text = document.getElementById("myBox");
+      console.log("Copy process Achive");
+      text.select();
+      navigator.clipboard.writeText(text.value);
+      
+    }
     const EventOnChange = (event) =>{
         console.log("On Change");
         setText(event.target.value);
     }
   return (
-    
-    <div>
+    <>
+    <div className = "container" style = {{color:props.mode==='light'?'black':'white'}}>
         
-        <h1>{props.heading}</h1>
+        <h1 my-5>{props.heading}</h1>
         <div className="mb-3">
             
-            <textarea className="form-control" onChange = {EventOnChange} id="myBox" value={text} rows="12"></textarea>
+            <textarea className="form-control" onChange = {EventOnChange} style = {{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}} id="myBox" value={text} rows="12"></textarea>
         </div>
-        <button className="btn btn-primary"  onClick={EventUp} >Convert to upper case</button>
+        <button className="btn btn-primary mx-3 my-3"  onClick={EventUp} >Convert upper case</button>
+        <button className="btn btn-primary mx-3 my-3" onClick={EventDown} >Convert lower case</button>
+        <button onClick={speak} className="btn btn-warning mx-2 my-2">Speak</button>
+        <button className="btn btn-primary mx-3 my-2" onClick={copy}>Copy Text</button>
+        <button className="btn btn-danger mx-3" onClick={clear}>Clear</button>
   </div>
+  <div className = "container my-4" style = {{color:props.mode==='light'?'black':'white'}}>
+    <h1>Text Summary</h1>
+    <p>{text.split(" ").length-1} words and {text.length} characters</p>
+    <p>Time require to read {0.008*(text.split(" ").length-1)} minutes</p>
+    
+
+    <h2>Preview</h2>
+    <p>{text}</p>
+  </div>
+  </>
+
   )
 }
